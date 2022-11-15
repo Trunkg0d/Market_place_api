d7e5146fab68b3620866853e454e1c4db70fd057
Trunkg0d <trungsanglong@gmail.com>
2022-11-15 19:47:23 +0700

    Adds pagination for the products index action to optimize response

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
     before_action :check_login, only: [:create]
     before_action :check_owner, only: [:update, :destroy]
+    include Paginable
     
     def show
         @product = Product.find(params[:id])
@@ -9,8 +10,16 @@ class Api::V1::ProductsController < ApplicationController
     end
 
     def index
-        @products = Product.all
-        render json: ProductSerializer.new(@products).serializable_hash
+        @products = Product.page(params[:page]).per(params[:per_page]).search(params)
+        options = {
+            links: {
+                first: api_v1_products_path(page: 1),
+                last: api_v1_products_path(page: @products.total_pages),
+                prev: api_v1_products_path(page: @products.prev_page),
+                next: api_v1_products_path(page: @products.next_page),
+            }              
+        }
+        render json: ProductSerializer.new(@products, options).serializable_hash
     end
 
     def create
